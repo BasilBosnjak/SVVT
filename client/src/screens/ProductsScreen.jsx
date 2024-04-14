29b490ede07d307674e9ad47b3ae774d594c9a80
muky1 <muhamed.begic01@gmail.com>
2024-04-14 19:02:00 +0200

    Add ProductsScreen page

---
+++ client/src/screens/ProductsScreen.jsx
@@ -0,0 +1,44 @@
+import { Box, Wrap, WrapItem, Center } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+import { useDispatch, useSelector } from "react-redux";
+
+import ProductCard from "../components/ProductCard";
+import { getProducts } from "../redux/actions/productActions";
+
+const ProductsScreen = () => {
+  const dispatch = useDispatch();
+  const { loading, error, products, pagination } = useSelector(
+    (state) => state.product
+  );
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
+  return (
+    <>
+      {products.length > 1 && (
+        <Box>
+          <Wrap
+            spacing={"30px"}
+            justify={"center"}
+            minHeight={"80vh"}
+            mx={{ base: 12, md: 20, lg: 32 }}
+          >
+            {products.map((product) => (
+              <WrapItem key={product._id}>
+                <Center width={250} height={450}>
+                  <ProductCard product={product} isLoading={loading} />
+                </Center>
+              </WrapItem>
+            ))}
+          </Wrap>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default ProductsScreen;
