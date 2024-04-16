e0885a38712fe282f83edafbeb30a24c8716c1c2
muky1 <muhamed.begic01@gmail.com>
2024-04-16 23:54:16 +0200

    Update getProducts route with pagination

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -3,12 +3,27 @@ import Product from "../models/Product.js";
 
 const productRoutes = express.Router();
 
-const getProducts = async(req, res) => {
-    const products = await Product.find({});
+const getProducts = async (req, res) => {
+  const page = parseInt(req.params.page); // pages, ex: 1, 2, 3
+  const limit = parseInt(req.params.limit); // items per page, ex: 10, 15, 20
 
-    res.json(products);
-}
+  const products = await Product.find({});
 
-productRoutes.route('/').get(getProducts);
+  if (page && limit) {
+    const totalPages = Math.ceil(products.length / limit);
+    const startIndex = (page - 1) * limit;
+    const endIndex = startIndex + limit;
+    const paginatedProducts = products.slice(startIndex, endIndex);
+    res.json({
+      products: paginatedProducts,
+      pagination: { currentPage: page, totalPages },
+    });
+  } else {
+    res.json({ products, pagination: {} });
+  }
+};
+
+productRoutes.route("/").get(getProducts);
+productRoutes.route("/:page/:limit").get(getProducts);
 
 export default productRoutes;
