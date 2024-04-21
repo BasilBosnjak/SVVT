3bdf475628ef3d326616e04e7c3c77e913f598a8
muky1 <muhamed.begic01@gmail.com>
2024-04-21 03:45:56 +0200

    Refactor Product schema

--- server/models/Product.js
+++ server/models/Product.js
@@ -1,31 +1,48 @@
 import mongoose, { Schema } from "mongoose";
 
-const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    images: {
-        type: Array,
-        required: true,
-        default: []
-    },
-    categoryId: {
-        type: Schema.Types.ObjectId,
-        required: true,
-        ref: 'Category'
-    },
-}, {timestamps: true}
+const reviewSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+    title: { type: String, required: true },
+    // TODO: add ref to user schema later after implementing user schema
+  },
+  { timestamps: true }
 );
 
-const Product = mongoose.model('Product', productSchema);
+const productSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    reviews: [reviewSchema],
+    images: {
+      type: Array,
+      required: true,
+      default: [],
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Product = mongoose.model("Product", productSchema);
 
 export default Product;
