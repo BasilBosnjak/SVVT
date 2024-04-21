af23b9f580cacde15f65fe41e737c12616f4ebcb
muky1 <muhamed.begic01@gmail.com>
2024-04-21 03:38:07 +0200

    Add two more routes for products to ReactRouter

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { ChakraProvider } from "@chakra-ui/react";
 import ProductsScreen from "./screens/ProductsScreen";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import { Header } from "./components/Header";
+import ProductScreen from "./screens/ProductScreen";
+import LandingScreen from "./screens/LandingScreen";
 
 function App() {
   return (
@@ -10,7 +12,9 @@ function App() {
         <Header />
         <main>
           <Routes>
-            <Route path={"/"} element={<ProductsScreen />} />
+            <Route path={"/"} element={<LandingScreen />} />
+            <Route path={"/products"} element={<ProductsScreen />} />
+            <Route path={"/product/:id"} element={<ProductScreen />} />
           </Routes>
         </main>
       </Router>
