efd7b4084c8ba77ea3a6ebe05cf4cd86ea2e14c2
muky1 <muhamed.begic01@gmail.com>
2024-05-22 01:00:33 +0200

    Implement endpoints for users

--- server/index.js
+++ server/index.js
@@ -1,25 +1,27 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import databaseConnection from './db.js';
+import express from "express";
+import cors from "cors";
+import databaseConnection from "./db.js";
 
 // Routes
-import productRoutes from './routes/productRoutes.js';
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 databaseConnection();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/api/products', productRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 const PORT = 5001;
 
-app.get('/', (req, res) => {
-    res.send("API is running...");
-})
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port: ${PORT}`);
-})
+  console.log(`Server is listening on port: ${PORT}`);
+});
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,136 @@
+import express from "express";
+import User from "../models/User.js";
+import expressAsyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import { sendVerificationEmail } from "../middleware/sendVerificationEmail.js";
+import { sendPasswordResetEmail } from "../middleware/sendPasswordResetEmail.js";
+
+const userRoutes = express.Router();
+
+const genToken = (id) => {
+  return jwt.sign({ id }, process.env.TOKEN_SECRET, { expiresIn: "60d" }); // change expiresIn value before deploying live
+};
+
+const loginUser = expressAsyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPasswords(password))) {
+    user.firstLogin = false;
+    await user.save();
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      googleImage: user.googleImage,
+      googleId: user.googleId,
+      isAdmin: user.isAdmin,
+      token: genToken(user._id),
+      firstLogin: user.firstLogin,
+      created: user.createdAt,
+    });
+  } else {
+    res.status(401).send("Invalid Email or Password!");
+    throw new Error("User not found!");
+  }
+});
+
+const registerUser = expressAsyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400).send("User with that email already exists!");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  const newToken = genToken(user._id);
+
+  sendVerificationEmail(newToken, email, name, user._id);
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      googleImage: user.googleImage,
+      googleId: user.googleId,
+      firstLogin: user.firstLogin,
+      isAdmin: user.isAdmin,
+      token: newToken,
+      active: user.active,
+      createdAt: user.createdAt,
+    });
+  } else {
+    res.status(400).send("Unable to register!");
+    throw new Error(
+      "Something went wrong. Please check your credentials and try again!"
+    );
+  }
+});
+
+const verifyEmail = expressAsyncHandler(async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    const user = await User.findById(decoded.id);
+
+    if (user) {
+      user.active = true;
+      await user.save();
+      res.json(
+        "Account activated successfully, you can close this window now."
+      );
+    } else {
+      res.status(404).send("User not found!");
+    }
+  } catch (error) {
+    res.status(401).send("Email address could not be verified.");
+  }
+});
+
+const passwordResetRequest = expressAsyncHandler(async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await User.findOne({ email: email });
+
+    if (user) {
+      const newToken = genToken(user._id);
+      sendPasswordResetEmail(newToken, user.email, user.name);
+      res.status(200).send(`Password recovery email sent to ${email}`);
+    }
+  } catch (error) {
+    res.status(401).send("Account with that email doesn't exist.");
+  }
+});
+
+const passwordReset = expressAsyncHandler(async (req, res) => {
+  const token = req.headers.authorization.split(" ")[1];
+  try {
+    const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+    const user = await User.findById(decoded.id);
+
+    if (user) {
+      user.password = req.body.password;
+      await user.save();
+      res.json("Password updated successfully.");
+    } else {
+      res.status(404).send("User not found!");
+    }
+  } catch (error) {
+    res.status(401).send("Password reset failed");
+  }
+});
+
+userRoutes.route("/login").post(loginUser);
+userRoutes.route("/register").post(registerUser);
+userRoutes.route("/verify-email").get(verifyEmail);
+userRoutes.route("/password-reset-request").post(passwordResetRequest);
+userRoutes.route("/password-reset").post(passwordReset);
+
+export default userRoutes;
