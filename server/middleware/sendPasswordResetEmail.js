c8a8deccd1cadcddb16088879d005a6868e0152a
muky1 <muhamed.begic01@gmail.com>
2024-05-22 00:56:25 +0200

    Implement password reset email middleware

---
+++ server/middleware/sendPasswordResetEmail.js
@@ -0,0 +1,38 @@
+import nodemailer from "nodemailer";
+
+export const sendPasswordResetEmail = (token, email, name) => {
+  const html = `
+        <html>
+            <body>
+            <h2>Dear ${name}</h2>
+            <p>This is a password recovery email.</p>
+            <p>Please use the link below to reset your password!</p>
+            <a href="http://localhost:3000/password-reset/${token}">Click here.</a>
+            </body>
+        </html>
+    `;
+
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "muhamed.begic01@gmail.com",
+      pass: "nnsf mkuu beuv uyji",
+    },
+  });
+
+  const mailOptions = {
+    from: "Shopium <noreply.muhamed.begic01@gmail.com>",
+    to: email,
+    subject: "Password recovery",
+    html: html,
+    replyTo: "noreply.muhamed.begic01@gmail.com",
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error("Error sending email: ", error);
+    } else {
+      console.log(`Email sent to: ${email}`, info.response);
+    }
+  });
+};
