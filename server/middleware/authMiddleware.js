6a292de4928de2660434fa8a92fa3433314a6e57
muky1 <muhamed.begic01@gmail.com>
2024-05-29 02:32:49 +0200

    Implement authorization middleware

---
+++ server/middleware/authMiddleware.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import expressAsyncHandler from "express-async-handler";
+import User from "../models/User.js";
+
+const protectRoute = expressAsyncHandler(async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith(`Bearer`)
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
+      req.user = await User.findById(decoded.id);
+
+      next();
+    } catch (error) {
+      res.status(401);
+      throw new Error("Unauthorized, authentication failed.");
+    }
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Unauthorized, missing token.");
+  }
+});
+
+export { protectRoute };
