fdef39426a28ed60e04edaffa92390f2366ddb5d
muky1 <muhamed.begic01@gmail.com>
2024-07-12 06:02:26 +0200

    Wrap whole app in chakra provider instead of just router

--- client/src/App.js
+++ client/src/App.js
@@ -22,49 +22,51 @@ import AdminPanelScreen from "./screens/AdminPanelScreen";
 function App() {
   const googleClient = useGoogleClient();
 
-  return !googleClient ? (
-    <VStack paddingTop={"38vh"}>
-      <Spinner
-        marginTop={"20"}
-        thickness="2px"
-        speed="0.55s"
-        emptyColor="gray.300"
-        color="cyan.500"
-        size={"xl"}
-      />
-    </VStack>
-  ) : (
-    <GoogleOAuthProvider clientId={googleClient}>
-      <ChakraProvider>
-        <Router>
-          <Header />
-          <main>
-            <Routes>
-              <Route path={"/"} element={<LandingScreen />} />
-              <Route path={"/products"} element={<ProductsScreen />} />
-              <Route path={"/product/:id"} element={<ProductScreen />} />
-              <Route path={"/cart"} element={<CartScreen />} />
-              <Route path={"/login"} element={<LoginScreen />} />
-              <Route path={"/register"} element={<RegisterScreen />} />
-              <Route
-                path={"/email-verify/:token"}
-                element={<EmailVerificationScreen />}
-              />
-              <Route
-                path={"/password-reset/:token"}
-                element={<PasswordResetScreen />}
-              />
-              <Route path={"/success"} element={<SuccessScreen />} />
-              <Route path={"/order-history"} element={<OrdersScreen />} />
-              <Route path={"/cancel"} element={<CancelScreen />} />
-              <Route path={"/checkout"} element={<CheckoutScreen />} />
-              <Route path={"/admin-panel"} element={<AdminPanelScreen />} />
-            </Routes>
-          </main>
-          <Footer />
-        </Router>
-      </ChakraProvider>
-    </GoogleOAuthProvider>
+  return (
+    <ChakraProvider>
+      {!googleClient ? (
+        <VStack paddingTop={"38vh"}>
+          <Spinner
+            marginTop={"20"}
+            thickness="2px"
+            speed="0.55s"
+            emptyColor="gray.300"
+            color="cyan.500"
+            size={"xl"}
+          />
+        </VStack>
+      ) : (
+        <GoogleOAuthProvider clientId={googleClient}>
+          <Router>
+            <Header />
+            <main>
+              <Routes>
+                <Route path={"/"} element={<LandingScreen />} />
+                <Route path={"/products"} element={<ProductsScreen />} />
+                <Route path={"/product/:id"} element={<ProductScreen />} />
+                <Route path={"/cart"} element={<CartScreen />} />
+                <Route path={"/login"} element={<LoginScreen />} />
+                <Route path={"/register"} element={<RegisterScreen />} />
+                <Route
+                  path={"/email-verify/:token"}
+                  element={<EmailVerificationScreen />}
+                />
+                <Route
+                  path={"/password-reset/:token"}
+                  element={<PasswordResetScreen />}
+                />
+                <Route path={"/success"} element={<SuccessScreen />} />
+                <Route path={"/order-history"} element={<OrdersScreen />} />
+                <Route path={"/cancel"} element={<CancelScreen />} />
+                <Route path={"/checkout"} element={<CheckoutScreen />} />
+                <Route path={"/admin-panel"} element={<AdminPanelScreen />} />
+              </Routes>
+            </main>
+            <Footer />
+          </Router>
+        </GoogleOAuthProvider>
+      )}
+    </ChakraProvider>
   );
 }
 
